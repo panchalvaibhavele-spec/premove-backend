15f4290a000ad6bdd4f5679b908d680d2852a20a
panchalvaibhavele-spec <panchalvaibhavele@gmail.com>
2025-10-17 14:43:39 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,24 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
-  host: "localhost",
-  // port: 3306,          // change to 3306 if that's your MySQL port
-  user: "root",
-  password: "root",
-  database: "premove",
+const db = mysql.createPool({
+
+//     DB_HOST=srv917.hstgr.io
+// DB_PORT=3306
+// DB_USER=u874477730_unitedgulf
+// DB_PASS=O$y2W$=[6!N#
+// DB_NAME=u874477730_unitedgulf
+
+  host: "srv917.hstgr.io",
+  port: 3306,          // change to 3306 if that's your MySQL port
+  user: "u874477730_unitedgulf",
+  password: "O$y2W$=[6!N#",
+  database: "u874477730_unitedgulf",
   // waitForConnections: true,
   // connectionLimit: 10,   // adjust based on load
   // queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err) => {
   if (err) {
     console.error("❌ Database connect error:", err);
   } else {
