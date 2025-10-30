170ff517f84129b49516226c7f2eea5a3508c67f
panchalvaibhavele-spec <panchalvaibhavele@gmail.com>
2025-10-30 18:41:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Use Render's dynamic port or fallback to 5000
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, "0.0.0.0", () => {
+const PORT = 5000;
+server.listen(PORT() => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
