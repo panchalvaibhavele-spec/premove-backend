ee20d1abc9a5387feda98b6aeb2c54410e075448
panchalvaibhavele-spec <panchalvaibhavele@gmail.com>
2025-10-31 09:57:03 +0530

    Update baseurl.js

--- config/baseurl.js
+++ config/baseurl.js
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://premove-backend.onrender.com/api/",
+  baseURL: "https://premove-backend-14.onrender.com/api/",
   // baseURL: "http://192.168.0.155:5000/api/",
 });
