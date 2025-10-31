3bc615800d1073b90959029a3941f7ef5671314a
panchalvaibhavele-spec <panchalvaibhavele@gmail.com>
2025-10-31 16:06:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
+  password: process.env.DB_PASS || "root",
   database: process.env.DB_NAME || "premove",
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONN_LIMIT) || 10,
