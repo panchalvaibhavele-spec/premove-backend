c9b362ef474f11f9a80c3fb4255df3f3481ba9ee
panchalvaibhavele-spec <panchalvaibhavele@gmail.com>
2025-11-26 10:29:07 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { sendOtp, verifyOtp, checkJwt} = require("../controllers/authController");
+const { sendOtp, verifyOtp, checkJwt, registerCustomer, verifyRegisterOtp} = require("../controllers/authController");
 
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
 router.get("/check-jwt", checkJwt);
-
+router.post("/register-customer", registerCustomer);
+router.post("/verify-register-otp", verifyRegisterOtp);
 module.exports = router;
